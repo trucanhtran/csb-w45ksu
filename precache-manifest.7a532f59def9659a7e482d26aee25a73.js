5abef82598ecb74d09c1ba993461cfe1670e9876
Code Sandbox <hello@codesandbox.io>
2022-09-16 09:29:24 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7a532f59def9659a7e482d26aee25a73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ed25dcd1c31463ade3e12b0df1e473",
+    "url": "https://trucanhtran.github.io/csb-w45ksu/index.html"
+  },
+  {
+    "revision": "32b6444e9dd7c95ccf75",
+    "url": "https://trucanhtran.github.io/csb-w45ksu/static/css/main.279d0ea2.chunk.css"
+  },
+  {
+    "revision": "26c10a7ecb670f03ce42",
+    "url": "https://trucanhtran.github.io/csb-w45ksu/static/js/2.9cf51a5d.chunk.js"
+  },
+  {
+    "revision": "32b6444e9dd7c95ccf75",
+    "url": "https://trucanhtran.github.io/csb-w45ksu/static/js/main.f7d1d15c.chunk.js"
+  },
+  {
+    "revision": "137f844f6e1bd8e36023",
+    "url": "https://trucanhtran.github.io/csb-w45ksu/static/js/runtime~main.b8a2c1eb.js"
+  }
+]);
